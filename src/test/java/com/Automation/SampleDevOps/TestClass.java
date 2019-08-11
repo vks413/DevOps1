5d28b69e4cb254694cd4b131d8f2d654b3c4cb00
vks413 <53976020+vks413@users.noreply.github.com>
2019-08-11 19:28:04 +0530

    Change in java class as a comment

--- src/test/java/com/Automation/SampleDevOps/TestClass.java
+++ src/test/java/com/Automation/SampleDevOps/TestClass.java
@@ -8,6 +8,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+//Class for testing
 public class TestClass {
 	
 public static WebDriver driver;
