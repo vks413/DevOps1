51d1d7d0ad0e627ad913a3112a44f834aacfb9b9
unknown <vks413@yahoo.com>
2019-08-11 19:24:23 +0530

    first commit by Thakur

---
+++ src/main/java/com/Automation/SampleDevOps/App.java
@@ -0,0 +1,13 @@
+package com.Automation.SampleDevOps;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/Automation/SampleDevOps/TestClass.java
@@ -0,0 +1,43 @@
+package com.Automation.SampleDevOps;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class TestClass {
+	
+public static WebDriver driver;
+
+@BeforeMethod
+public static void launchBrowse()
+{
+	System.setProperty("webdriver.chrome.driver", "F:\\System Eclipse Projects\\SeleniumTest\\chromedriver.exe");
+	driver = new ChromeDriver();
+	driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+}
+
+@Test
+public static void methodTest1()
+{
+	driver.get("https://www.google.co.in/");
+	System.out.println("1 method");
+}
+
+@Test
+public static void methodTest2()
+{
+	driver.get("https://www.google.co.in/");
+	System.out.println("2 method");
+}
+
+@AfterMethod
+public static void closeBrowser()
+{
+	driver.close();
+}
+
+}
